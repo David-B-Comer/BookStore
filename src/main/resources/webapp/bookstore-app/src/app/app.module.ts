1fc32c8f85f71aee33ad0e799ad7dda3b385b902
David-B-Comer <davec2233@gmail.com>
2021-01-10 15:33:56 -0500

    added add book

--- src/main/resources/webapp/bookstore-app/src/app/app.module.ts
+++ src/main/resources/webapp/bookstore-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './book/add-book/add-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent
   ],
   imports: [
     BrowserModule,
